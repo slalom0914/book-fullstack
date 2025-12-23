9267f8dc3de696aca26427430926828b757a029c
키위 <slalom0914@gmail.com>
2025-12-23 18:28:38 +0900

    8교시

--- front-end/src/service/deptLogic.js
+++ front-end/src/service/deptLogic.js
@@ -1,4 +1,19 @@
 import axios from 'axios'
+// 서블릿에게 부서 등록 요청하기
+export const deptInsertDB = async (data) => {
+  console.log(data)
+  try {
+    const res = await axios({
+      method: 'post',
+      url: process.env.REACT_APP_TOMCAT_IP+'dept',
+      data: data
+    })
+    console.log(res.data)
+    return res
+  } catch (error) {
+    console.error("부서등록 실패!!!", error)    
+  }
+}//end of deptInsertDB
 
 export const deptListDB = async (data) => {
   console.log(data)
@@ -31,4 +46,19 @@ export const deptListDB2 = async (data) => {
     //호출한 쪽에서 catch하도록 다시 던져 줄때
     throw error
   }
-}//end of deptListDB
+}//end of deptListDB2
+
+export const deptListDB3 = async (params) => {
+  console.log('deptListDB3 호출')
+  console.log(params)
+  try {
+    //만일 파라미터로 값을 넘길 때 속성이름과 객체리터럴 변수명이 같다면 생략이 가능함.
+    const res = await axios.get(process.env.REACT_APP_TOMCAT_IP+'dept',{params})
+    console.log(res.data)
+    return res 
+  } catch (error) {
+    console.error("부서목록 가져오기 실패!!!", error)
+    //호출한 쪽에서 catch하도록 다시 던져 줄때
+    throw error
+  }
+}//end of deptListDB3
