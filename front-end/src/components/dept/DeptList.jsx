e301fdc5574b297572faa8b21cf2af93b0124586
키위 <slalom0914@gmail.com>
2026-01-06 18:07:53 +0900

    모달처리중- 8교시

--- front-end/src/components/dept/DeptList.jsx
+++ front-end/src/components/dept/DeptList.jsx
@@ -2,9 +2,14 @@ import { useEffect, useState } from 'react'
 import { deptListDB } from '../../service/deptLogic'
 import styles from './dept.module.css'
 import DeptRow from './DeptRow'
+import { Button, Modal } from 'react-bootstrap'
+import { MyInput, MyLabel, MyLabelAb } from '../style/FormStyle'
 
 const DeptList = () => {
   const [deptList, setDeptList] = useState([])
+  const [ deptno, setDeptno ] = useState(0)
+  const [ dname, setDname] = useState('')
+  const [ loc, setLoc] = useState('')
   useEffect(() => {
     const getDeptList = async() => {
       const dept = {
@@ -26,6 +31,29 @@ const DeptList = () => {
   const reactSearch = () => {
     
   }
+  //모달 관련 상태
+  const [ show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+  // 사용자가 입력한 부서 정보를 객체 리터럴로 담아서 서버에 전송한다.
+  // json -> 객체
+  // 객체 -> json
+  const deptInsert = async () => {
+
+  }
+  const handleDeptno = (value) => {
+    setDeptno(value)
+  }
+
+  const handleDname = (value) => {
+    setDname(value)
+  }
+
+  const handleLoc = (value) => {
+    setLoc(value)
+  }
+
   return (
     <>
       <div className="container">
@@ -76,25 +104,41 @@ const DeptList = () => {
         </div>
       </div>
       </div>
-      {/* ========================== [[ 부서등록 Modal ]] ========================== */}
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      {/* ========================== [[ 도서등록 Modal ]] ========================== */}
+      <Modal show={show} onHide={handleClose} animation={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>부서등록</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+          <div style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
+            <div style={{display: 'flex'}}>
+              <MyLabel>부서번호
+                <MyInput type="text" id="deptno" placeholder="Enter 부서번호" onChange={(e)=>{handleDeptno(e.target.value)}} />
+              </MyLabel>
             </div>
-            <div class="modal-body">
-              ...
+            <div style={{display: 'flex'}}>
+              <MyLabel>부서명
+                <MyInput type="text" id="dname" placeholder="Enter 부서명" onChange={(e)=>{handleDname(e.target.value)}}/>
+              </MyLabel>
             </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
+            <div style={{display: 'flex'}}>
+              <MyLabel>지역
+                <MyInput type="text" id="loc" placeholder="Enter 지역" onChange={(e)=>{handleLoc(e.target.value)}}/>
+              </MyLabel>
             </div>
           </div>
-        </div>
-      </div>
-      {/* ========================== [[ 부서등록 Modal ]] ========================== */}
+
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              닫기
+            </Button>
+            <Button variant="primary" onClick={deptInsert}>
+              저장
+            </Button>
+          </Modal.Footer>
+        </Modal>     
+      {/* ========================== [[ 부서등록 Modal ]] ========================== */}    
     </>
   )
 }
