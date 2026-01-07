de19eb2ab6ad53aa045ddcd2cecb9713c97d9f44
키위 <slalom0914@gmail.com>
2026-01-07 15:18:05 +0900

    조건검색 쿼리문 반영- 5교시

--- back-end/src/main/java/com/example/demo/vo/DeptVO.java
+++ back-end/src/main/java/com/example/demo/vo/DeptVO.java
@@ -7,4 +7,8 @@ public class DeptVO {
     private int deptno;
     private String dname;
     private String loc;
+
+    // 검색용 변수
+    private String searchType; //"deptno" | "dname" | "loc"
+    private String keyword; // 검색어
 }
